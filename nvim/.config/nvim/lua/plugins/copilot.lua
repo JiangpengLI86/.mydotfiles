2d972b25effb9f0af2de96ed719e15eb8931b3c7
JiangpengLI <jiangpengli86@gmail.com>
2024-09-18 16:50:04 +0800

    Edit Copilot configs in nvim

--- nvim/.config/nvim/lua/plugins/copilot.lua
+++ nvim/.config/nvim/lua/plugins/copilot.lua
@@ -1,24 +1,21 @@
 return {
-  "zbirenbaum/copilot.lua",
+    "zbirenbaum/copilot.lua",
 
-  -- Lazy-load on command
-  cmd = "Copilot",
-  -- build is exectued when a plugin is installed or udpated.
-  build = ":Copilot auth",
-  -- Lazy-load on event: InsertEnter
-  event = "InsertEnter",
+    -- Lazy-load on command
+    cmd = "Copilot",
+    -- build is exectued when a plugin is installed or udpated.
+    build = ":Copilot auth",
+    -- Lazy-load on event: InsertEnter
+    event = "InsertEnter",
 
-  config = function()
-    require("copilot").setup({})
-  end,
-  -- opts should be a table which will be passed to the config function as an argument.
-  -- Setting this function will imply plugin.config()
-  opts = {
-    suggestion = { enabled = false },
-    panel = { enabled = false },
-    filetypes = {
-      markdown = true,
-      help = true,
+    -- opts should be a table which will be passed to the config function as an argument.
+    -- Setting this function will imply plugin.config()
+    opts = {
+        suggestion = { enabled = false },
+        panel = { enabled = false },
+        filetypes = {
+            markdown = true,
+            help = true,
+        },
     },
-  },
 }
