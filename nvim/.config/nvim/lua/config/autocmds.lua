f72365c4acd22267789ceda7d523507a1c1f5f49
JiangpengLI <jiangpengli86@gmail.com>
2024-09-18 16:52:45 +0800

    Enable nvim buildin spell check

--- nvim/.config/nvim/lua/config/autocmds.lua
+++ nvim/.config/nvim/lua/config/autocmds.lua
@@ -1,3 +1,12 @@
 -- Autocmds are automatically loaded on the VeryLazy event
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
 -- Add any additional autocmds here
+
+-- Enable buildin spell checker
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = { "markdown", "text", "python", "bash", "lua", "cpp" },
+  callback = function()
+    vim.opt.spell = true -- Enable spell checking
+    vim.opt.spelllang = "en" -- Set the language (you can set multiple like "en,es")
+  end,
+})
