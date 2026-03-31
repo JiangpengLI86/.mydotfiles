c79f68b9c0801cf7e2c44067b820e755fbd536cd
JiangpengLI <jiangpengli86@gmail.com>
2026-03-31 14:18:24 +0800

    fix(lspconfig): use conditional guard and explicit return opts for clangd config

--- nvim/.config/nvim/lua/plugins/extend_nvim_lspconfig.lua
+++ nvim/.config/nvim/lua/plugins/extend_nvim_lspconfig.lua
@@ -3,21 +3,22 @@ return {
     opts = function(_, opts)
         local lsputil = require("lspconfig.util")
         local clangd_config = opts.servers.clangd
-        if not clangd_config then return end
-
-        clangd_config.root_dir = function(fname)
-            return lsputil.root_pattern(
-                "CMakeLists.txt",
-                "Makefile",
-                "configure.ac",
-                "configure.in",
-                "config.h.in",
-                "meson.build",
-                "meson_options.txt",
-                "build.ninja"
-            )(fname)
-                or lsputil.root_pattern("compile_commands.json", "compile_flags.txt")(fname)
-                or lsputil.find_git_ancestor(fname)
+        if clangd_config then
+            clangd_config.root_dir = function(fname)
+                return lsputil.root_pattern(
+                    "CMakeLists.txt",
+                    "Makefile",
+                    "configure.ac",
+                    "configure.in",
+                    "config.h.in",
+                    "meson.build",
+                    "meson_options.txt",
+                    "build.ninja"
+                )(fname)
+                    or lsputil.root_pattern("compile_commands.json", "compile_flags.txt")(fname)
+                    or lsputil.find_git_ancestor(fname)
+            end
         end
+        return opts
     end,
 }
